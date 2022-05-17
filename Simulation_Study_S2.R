ccf67ca28c589c5f5c6ea4bd10b780c3c1eb6b7f
cmadridp <105686855+cmadridp@users.noreply.github.com>
2022-05-17 16:10:03 -0400

    Update Simulation_Study_S2.R

--- Simulation_Study_S2.R
+++ Simulation_Study_S2.R
@@ -238,7 +238,7 @@ for(rep in 1:repetitions){
   delta_s<-matrix(NaN,nrow=N_t, ncol=1)
   m=max(nts)
   #m
-  #ruido gaussiano
+  #Gaussian noise
   sigma=matrix(NaN,nrow=m,ncol=m)
   for(i in 1:m){
     for(j in 1:m){
