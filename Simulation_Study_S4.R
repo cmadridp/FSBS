f77a55fb9e9dda74ae6396b6f65b8aa2ec0db614
cmadridp <105686855+cmadridp@users.noreply.github.com>
2022-05-17 16:15:45 -0400

    Update Simulation_Study_S4.R

--- Simulation_Study_S4.R
+++ Simulation_Study_S4.R
@@ -1,6 +1,6 @@
 rm(list=ls())
 
-#load libraries
+#Load libraries
 library(fda)
 library(ks)
 library(mvtnorm)
@@ -22,11 +22,11 @@ source("Competitors_Functions.R")
 Tb=200
 r=1
 C=3
-#the responses lies in dimension 3
+#The responses lies in dimension 3
 n=10
-#the observations lies in dimension 1
+#The observations lies in dimension 1
 d=2
-#we define the n_ts
+#We define the n_ts
 nts<-matrix(NaN,nrow=Tb, ncol=1)
 for(i in 1:Tb){
   nts[i]=n
@@ -36,7 +36,7 @@ N_t
 
 
 
-#we create our functions f*s
+#We create our functions f*s
 f1_star=function(x){
   return(0)
 }
@@ -63,7 +63,7 @@ count_iter=0
 
 for(rep in 1:repetitions){
   count_iter=count_iter+1
-  #we create the xs
+  #We create the xs
   X_s<-matrix(NaN,nrow=N_t, ncol=d)
   set.seed(rep)
   for(i in 1:N_t){
@@ -75,7 +75,7 @@ for(rep in 1:repetitions){
   #X_s
   
   
-  #we create the \xi_s--------------------------------------------
+  #We create the \xi_s--------------------------------------------
   val_in_0_1=0.5
   number_basis=50
   set.seed(rep)
@@ -87,19 +87,19 @@ for(rep in 1:repetitions){
     for (i in 1:n) {
       #xti is X_s[aux-nts[t]+i,]
       X_s[aux-nts[t]+i,]
-      #creating xi_0 at xti to be aux_2
+      #Creating xi_0 at xti to be aux_2
       b_t_0=b_t_js_0[,1]
       aux_2=0
       for (j in 1:number_basis) {
-        #creating h_j at x_ti to be aux_1
+        #Creating h_j at x_ti to be aux_1
         aux_1=1
         aux_1=(sqrt(2)/2*pi)*sin(j*X_s[aux-nts[t]+i,1])*(aux_1)*(1/45)*(sqrt(2)/2*pi)*cos((number_basis-j)*X_s[aux-nts[t]+i,2])
         aux_2=aux_2+b_t_0[j]*(aux_1)
       }
-      #creating xi_t at xti to be aux_3
+      #Creating xi_t at xti to be aux_3
       aux_3=val_in_0_1^t*(aux_2)
       for(l in 1:t){
-        #we need xi_t_aux(x,i) to be aux_4
+        #We need xi_t_aux(x,i) to be aux_4
         b_t_0_js=b_t_js_0[,l]
         aux_4=0
         for (j in 1:number_basis) {
@@ -113,11 +113,11 @@ for(rep in 1:repetitions){
       xi_t_matrix[i,t]=aux_3
     }
   }
-  #we create the delta_ts--------------------------------------
+  #We create the delta_ts--------------------------------------
   delta_s<-matrix(NaN,nrow=N_t, ncol=1)
   m=max(nts)
   #m
-  #ruido gaussiano
+  #Gaussiano noise
   sigma=matrix(NaN,nrow=m,ncol=m)
   for(i in 1:m){
     for(j in 1:m){
@@ -135,10 +135,10 @@ for(rep in 1:repetitions){
   }
   set.seed(rep)
   normal_noise <- rmvnorm(Tb, mean=mean, sigma=sigma)
-  #normal_noise
+  #Normal_noise
   normal_noise=t(normal_noise)
-  #normal_noise
-  #matrix A
+  #Normal_noise
+  #Matrix A
   A=diag(0.3,m,m)
   
   #delta_s_0=rmvnorm(1, mean=mean, sigma=diag(1,m,m))
@@ -156,7 +156,7 @@ for(rep in 1:repetitions){
   
   
   
-  #we create the y_i_ts----------------------------------------
+  #We create the y_i_ts----------------------------------------
   y_t_is=matrix(NaN,nrow=N_t,ncol=1)
   aux=0
   for(t in 1:Tb)
@@ -189,7 +189,7 @@ for(rep in 1:repetitions){
   y_t_is
   
   
-  #we create the data matrix containing x_t_is, y_t_is and labels
+  #We create the data matrix containing x_t_is, y_t_is and labels
   labels_Ts=matrix(NaN,nrow=N_t,ncol=1)
   aux1=0
   for (i in 1:Tb) {
@@ -198,7 +198,7 @@ for(rep in 1:repetitions){
       labels_Ts[aux1-nts[i]+j]=i
     }
   }
-  #labels_Ts
+  #Labels_Ts
   dat=matrix(NaN,nrow=N_t)
   dat=cbind(labels_Ts,X_s)
   dat=cbind(dat,y_t_is)
@@ -207,8 +207,8 @@ for(rep in 1:repetitions){
   
   
   
-  #using the data---------------------------------------------------
-  #we create the seeded intervals
+  #Using the data---------------------------------------------------
+  #We create the seeded intervals
   s.inter<-seeded.intervals(Tb,C)
   s.inter
   
@@ -216,49 +216,32 @@ for(rep in 1:repetitions){
   #s.inter_train
   
   
-  #the choice of bandwidth h_bar
+  #The choice of bandwidth h_bar
   H_bar_stimated=Hpi.diag(X_s)
   H_bar_stimated
   h_bar_aux=H_bar_stimated
   h_bar=sqrt(mean(diag(h_bar_aux)))
   
   
-  #we define tau and h
-  #tau_1=6.416
-  #h=1
-  #tau=(7)*log(Tb)*sqrt(1/(n*(h^2))+1)
-  #tau
-  #we call our algorithm
-  #start_time <- Sys.time()
-  #S_1=seedBS(dat, s.inter, h, h_bar, tau, d,n )
-  #end_time <- Sys.time()
-  #end_time - start_time
-  #S_1
-  
-  
-  
   
   ######################################################
   ######################### CV #########################
   ######################################################
-  #creating testing and training sets.
+  #Creating testing and training sets.
   dat_train=dat[(n+1):(2*n),]
   dat_test=dat[1:n,]
   for(i in 1:(Tb/2-1)){
     dat_test=rbind(dat_test,dat[(2*i*n+1):((2*i+1)*n),])
     dat_train=rbind(dat_train,dat[((2*i+1)*n+1):((2*(i+1))*n),])
   }
-  #dat_test
-  #dat_train
   dat_train[,1]=ceiling(dat_train[,1]/2)
-  #dat_train
   
-  #we create the seeded intervals to work with training data
+  #We create the seeded intervals to work with training data
   s.inter_train<-seeded.intervals(Tb/2,C)
-  #s.inter_train
+
   
   
-  #we create the estimator functions 
+  #We create the estimator functions 
   g_hat_i=function(eta1,eta2,h,x,dat_train,h_bar,phat)
   {
     res=0
@@ -269,13 +252,13 @@ for(rep in 1:repetitions){
     return(res)
   }
   
-  #we create the possible values for h and tau
+  #We create the possible values for h and tau
   h_int=seq(0.99,1.01,0.01)
   l_h_int=length(h_int)
   tau_int=c(.445,.45,.455)
   l_tau_int=length(tau_int)
   
-  #we compute errors of estimation
+  #We compute errors of estimation
   
   errors=matrix(NaN,l_h_int,l_tau_int)
   #start_time <- Sys.time()
@@ -313,8 +296,8 @@ for(rep in 1:repetitions){
   h_min=h_int[h_min]
   tau_min=max(h_tau[,2])
   tau_min=tau_int[tau_min]
-  #using the data---------------------------------------------------
-  #we create the seeded intervals
+  #Using the data---------------------------------------------------
+  #We create the seeded intervals
   s.inter<-seeded.intervals(Tb,C)
   S_1=seedBS(dat, s.inter, h_min, h_bar, tau_min, d,n )
   aux_matrix=matrix(NaN,length(S_1),2)
